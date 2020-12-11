714bc8295f1c81b4b066d3591af86368ab94b9c4
Bahram Movlanov <movlan@users.noreply.github.com>
2020-12-10 18:29:47 -0700

    get length

---
+++ Contributors/BahramMovlanov/getLength/getLength.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const getLength = (arr) => {
+  // if ()
+  // let keys = arr.keys()
+  // return [...keys].pop() + 1
+}
+
+console.log(getLength([1])) // 1
+console.log(getLength([1, 2])) // 2
+console.log(getLength([1, 2, 3, 4, 5])) // 5
+console.log(getLength([])) // 0
+console.log(getLength([undefined])) // 1
